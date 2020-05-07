b5221b46190dcb0c9d05bfd5c1d4cfdc12751697
unknown <himanshusingh1028@gmail.com>
2020-05-07 23:33:42 +0530

    Added on about page

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 //"http://localhost:3000/weather?address=
-    fetch( "/weather?address="+encodeURIComponent(location)).then( (response) => {
+    fetch( "http://localhost:3000/weather?address="+encodeURIComponent(location)).then( (response) => {
     response.json().then( (data) => {
         if( data.error ){
             messageOne.textContent = data.error
